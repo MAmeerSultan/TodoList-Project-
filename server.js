b73ab3d8f69457c3b2bb5a9c7410e005a7dbd305
Ameer Sultan <ameer.sultan47@outlook.com>
2023-11-26 12:25:29 +0000

    Added custom list functionality

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
+const mongoose = require("mongoose")
+const _ = require("lodash")
 
-let itemList = ["Buy Food.", "Cook Food.", "Eat Food."];
 let workList = [];
 const app = express();
 
@@ -10,22 +11,77 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
 app.set('view engine', 'ejs')
 
-let options = {
-    weekday: "long",
-    month: "long",
-    day: "numeric"
-}
-let Day = new Date()
-let currentDay = Day.toLocaleDateString("en-US", options)
+
+
+
+
+  mongoose.connect('mongodb://127.0.0.1:27017/todolist');
+  const itemSchema = new mongoose.Schema({
+    name:String
+  })
+  const Item = mongoose.model("Item", itemSchema)
+
+  const Item1 = new Item({
+    name: "Welcome to your todo list."
+  })
+  const Item2 = new Item({
+    name: "Hit the + button to add an item."
+  })
+  const Item3 = new Item({
+    name: "<-- Hit this to delete an item."
+  })
+
+  const defaultItems = [Item1, Item2, Item3]
+
+ const listSchema = new mongoose.Schema({
+    name: String, 
+    items: [itemSchema]
+ })
+
+ const List = mongoose.model("List", listSchema)
+
+
+
+
+
 
 app.get("/", function(req, res){
-    
+    Item.find({}).then(function(results){
+        if (results.length === 0){
+            Item.insertMany(defaultItems).then(function(){
+                console.log("Database Successfully Updated")
+              }).catch(function(err){
+                console.log(err)
+              })
+              res.redirect("/")
+        }
+        else{
+            res.render("list", {listTitle: "Today", newItems:results})
+        }
+        
+    }).catch(function(err){
+        console.log(err)
+    })
 
-    res.render("list", {listTitle: currentDay, newItems:itemList})
+    
 })
 
-app.get("/work", function(req, res){
-    res.render("list", {listTitle: "work List", newItems: workList})
+app.get("/:ListName", function(req, res){
+    const ListName = _.capitalize(req.params.ListName) 
+    List.findOne({name: ListName}).then(function(result){
+        if(!result){
+            const customList = new List({
+                name:ListName, 
+                items:defaultItems
+            })
+            customList.save()
+            res.redirect("/" + ListName)
+        }else{
+            res.render("list", {listTitle: result.name, newItems:result.items})
+        }
+    })
+   
+    // res.render("list", {listTitle: ListName, newItems : })
 })
 
 // app.post("/work", function(req, res){
@@ -41,28 +97,65 @@ app.get("/work", function(req, res){
 // })
 
 app.post("/", function(req, res){
-    let newItem = req.body.listItem
-    if (req.body.list === "work List"){
-        for (var i = 0; i<workList.length; i++){
-            if (newItem === workList[i]){
-                res.send("This Item Already Exist In The List.")
-                return 
-            }
-        }
-        workList.push(newItem)
-        res.redirect("/work")
-    }
-    else{
-        for (var i = 0; i<itemList.length; i++){
-            if (newItem === itemList[i]){
-                res.send("This Item Already Exist In The List.")
-                return 
-            }
-        }
-        itemList.push(newItem)
-        res.redirect("/")
+    const newItem = req.body.listItem
+    const ListName = req.body.list
+
+   
+
+    const added_Item = new Item({
+        name: newItem
+    })
+    if(ListName === "Today"){
+        added_Item.save().then(function(){
+            res.redirect("/")
+        })
+    }else{
+        List.findOne({name:ListName }).then(function(result){
+            result.items.push(added_Item)
+            result.save()
+            res.redirect("/" + ListName)
+        })
     }
    
+    // if (req.body.list === "work List"){
+    //     for (var i = 0; i<workList.length; i++){
+    //         if (newItem === workList[i]){
+    //             res.send("This Item Already Exist In The List.")
+    //             return 
+    //         }
+    //     }
+    //     workList.push(newItem)
+    //     res.redirect("/work")
+    // }
+    // else{
+    //     for (var i = 0; i<itemList.length; i++){
+    //         if (newItem === itemList[i]){
+    //             res.send("This Item Already Exist In The List.")
+    //             return 
+    //         }
+    //     }
+    //     itemList.push(newItem)
+    //     res.redirect("/")
+    // }
+   
+    
+})
+
+app.post("/delete", function(req, res){
+    const checkedItem = req.body.checkbox
+    const listName = req.body.listName
+
+    if (listName === "Today"){
+        Item.findByIdAndDelete(checkedItem).then(function(){
+            res.redirect("/")
+        })
+    }
+    else{
+        List.findOneAndUpdate({name: listName}, {$pull: {items: {_id : checkedItem}}}).then(function(){
+            res.redirect("/" + listName)
+        })
+    }
+
     
 })
 
